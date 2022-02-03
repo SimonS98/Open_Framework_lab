90f4f402b31e40ef3bc22121cb2531d6e41f1664
SimonS98 <80315339+SimonS98@users.noreply.github.com>
2022-02-03 15:02:08 +0000

    Uploaded

---
+++ Boids_Lab/Boids_2_1/src/testApp.cpp
@@ -0,0 +1,100 @@
+#include "testApp.h"
+
+const int boidsNum = 20;
+const int birdsNum = 20;
+
+testApp::~testApp()
+{
+	for (int i = 0; i < boids.size(); i++)
+	{
+		delete boids[i];
+	}
+    for (int i = 0; i < birds.size(); i++)
+    {
+        delete birds[i];
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	
+	
+	int screenW = ofGetScreenWidth();
+	int screenH = ofGetScreenHeight();
+
+	ofBackground(255,255,255);
+	
+	// set up the boids
+	for (int i = 0; i < boidsNum; i++)
+		boids.push_back(new Boid());
+    for (int i = 0; i < birdsNum; i++)
+        birds.push_back(new Bird());
+
+}
+
+
+//--------------------------------------------------------------
+void testApp::update(){
+	
+    ofVec3f min(0, 0);
+	ofVec3f max(ofGetWidth(), ofGetHeight());
+	for (int i = 0; i < boids.size(); i++)
+	{
+		boids[i]->update(boids, min, max);
+	}
+    
+    for (int i = 0; i < birds.size(); i++)
+    {
+        birds[i]->update(birds, min, max);
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+
+	for (int i = 0; i < boids.size(); i++)
+	{
+		boids[i]->drawboids();
+	}
+    for (int i = 0; i < birds.size(); i++)
+    {
+        birds[i]->drawbirds();
+    }
+    ofTriangle(100,100,200,200,300,300);
+}
+
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+ 
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+    
+}
